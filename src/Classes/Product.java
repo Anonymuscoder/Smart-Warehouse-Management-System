0041b70378b42f97206f1fcc257c3038af3a11fb
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2026-04-07 14:35:05 +0530

    product class added

---
+++ src/Classes/Product.java
@@ -0,0 +1,31 @@
+package Classes;
+
+public class Product {
+    int id;
+    String name;
+    int quantity;
+    double basePrice;
+
+    public Product(int id, String name, int quantity, double basePrice){
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.basePrice = basePrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+}
