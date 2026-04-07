d0f2bbd80561608beccb944655c1155744a7d8b5
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2026-04-07 15:11:11 +0530

    created Electronics class with member variables and constructors.

---
+++ src/Classes/Electronics.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class Electronics extends Product{
+    private int wattage;
+    private String warranty;
+
+    public Electronics(int id, String name, int quantity, double basePrice, int wattage, String warranty) {
+        super(id, name, quantity, basePrice);
+        this.wattage = wattage;
+        this.warranty = warranty;
+    }
+
+
+}
