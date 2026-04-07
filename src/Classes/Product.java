dc7689bca75a7f63a4519ad5e2b2729da2b93e4e
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2026-04-07 14:57:06 +0530

    made member variables private

--- src/Classes/Product.java
+++ src/Classes/Product.java
@@ -1,10 +1,10 @@
 package Classes;
 
 public class Product {
-    int id;
-    String name;
-    int quantity;
-    double basePrice;
+    private int id;
+    private String name;
+    private int quantity;
+    private double basePrice;
 
     public Product(int id, String name, int quantity, double basePrice){
         this.id = id;
